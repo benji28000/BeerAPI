0f7f3e747bd20784693ed4affbf8eebfe069107f
Benjamin Perez <95043947+benji28000@users.noreply.github.com>
2023-11-28 09:37:30 +0100

    refactor

--- beer/worker.js
+++ beer/worker.js
@@ -5,18 +5,18 @@ const cacheName = 'my-pwa-cache-v1';
 
 const resourcesToCache = [
 
-  'beer.html',
-  'script.js',
-  'addbeer.html',
-    'style.css',
+  '/beer/beer.html',
+  '/beer/script.js',
+  '/beer/addbeer.html',
+    '/beer/style.css',
 
-    '/images/favicon-32x32.png',
-    '/images/favicon-16x16.png',
-    '/images/apple-touch-icon.png',
-    '/images/android-chrome-192x192.png',
-    '/images/android-chrome-512x512.png',
-    '/images/favicon.ico',
-    '/images/mstile-150x150.png',
+    '/beer/images/favicon-32x32.png',
+    '/beer/images/favicon-16x16.png',
+    '/beer/images/apple-touch-icon.png',
+    '/beer/images/android-chrome-192x192.png',
+    '/beer/images/android-chrome-512x512.png',
+    '/beer/images/favicon.ico',
+    '/beer/images/mstile-150x150.png',
     
 ];
 
