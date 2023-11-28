b5f296016d26dc2fd6194cf669646a56a8ade4e8
Benjamin Perez <95043947+benji28000@users.noreply.github.com>
2023-11-28 09:29:37 +0100

    update cache images

--- beer/worker.js
+++ beer/worker.js
@@ -9,6 +9,15 @@ const resourcesToCache = [
   'script.js',
   'addbeer.html',
     'style.css',
+    'manifest.json',
+    '/images/favicon-32x32.png',
+    '/images/favicon-16x16.png',
+    '/images/apple-touch-icon.png',
+    '/images/android-chrome-192x192.png',
+    '/images/android-chrome-512x512.png',
+    '/images/favicon.ico',
+    '/images/mstile-150x150.png',
+    
 ];
 
 self.addEventListener('install', event => {
